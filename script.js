838170cf676d56d1fc74caffdbf88deb045d7b35
Kamila <pruskikamila@gmail.com>
2023-05-15 20:23:41 +0100

    updates on the layout + what-is page

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ window.addEventListener('scroll', function () {
 
 window.addEventListener('scroll', function () {
 	let Ynumber = window.pageYOffset;
-	console.log(Ynumber);
 	if (Ynumber > 60) {
 		mainnavIdeas.classList.add('hide-text');
 	} else {
@@ -33,94 +32,65 @@ window.addEventListener('scroll', function () {
 	}
 });
 
-const navFeatures = document.querySelector('#navfeatures');
-const navCertifications = document.querySelector('#navcertifications');
-const navMaterials = document.querySelector('#navmaterials');
+// code for whatIs page
 
-const pmaterials = document.querySelector('div.materials-text');
-const pcertifications = document.querySelector('div.certifications-text');
-const pfeatures = document.querySelector('div.features-text');
+const featuresnav = document.querySelector('#navfeatures');
+const certificationsnav = document.querySelector('#navcertifications');
+const materialsnav = document.querySelector('#navmaterials');
 
-const overFeatures = () => {
-	if (pfeatures.classList.contains('hide')) {
-		pfeatures.classList.add('show');
-		pfeatures.classList.remove('hide');
-	}
-	if (pmaterials.classList.contains('show')) {
-		pmaterials.classList.remove('show');
-		pfeatures.classList.add('show');
-	}
+const features = document.querySelector('.features-text');
+const materials = document.querySelector('.materials-text');
+const certifications = document.querySelector('.certifications-text');
 
-	if (pcertifications.classList.contains('show')) {
-		pfeatures.classList.add('show');
-		pfeatures.classList.remove('hide');
-	} else {
-		pfeatures.classList.add('show');
-	}
+const showfeatures = () => {
+	materials.style.opacity = 0;
+	certifications.style.opacity = 0;
+	features.style.opacity = 1;
 };
 
-const overMaterials = () => {
-	if (pmaterials.classList.contains('hide')) {
-		pmaterials.classList.remove('hide');
-		pmaterials.classList.add('show');
-	}
-
-	if (pfeatures.classList.contains('show')) {
-		pfeatures.classList.remove('show');
-		pmaterials.classList.add('show');
-	}
-	if (pcertifications.classList.contains('show')) {
-		pcertifications.classList.remove('show');
-		pmaterials.classList.add('show');
-	}
+const showcertifications = () => {
+	materials.style.opacity = 0;
+	features.style.opacity = 0;
+	certifications.style.opacity = 1;
 };
 
-function overCertficiations() {
-	if (pcertifications.classList.contains('hide')) {
-		pcertifications.classList.remove('hide');
-		pcertifications.classList.add('show');
-	}
-	if (pmaterials.classList.contains('show')) {
-		pmaterials.classList.remove('show');
-		pmaterials.classList.add('hide');
-		pcertifications.classList.add('show');
-	}
-	if (pfeatures.classList.contains('show')) {
-		pfeatures.classList.remove('show');
-		pfeatures.classList.add('hide');
-		pcertifications.classList.add('show');
-	}
+const showmaterials = () => {
+	materials.style.opacity = 1;
+	features.style.opacity = 0;
+	certifications.style.opacity = 0;
+};
+
+if (featuresnav != null) {
+	featuresnav.addEventListener('mouseover', showfeatures);
+}
+if (certificationsnav != null) {
+	certificationsnav.addEventListener('mouseover', showcertifications);
 }
 
-if (navFeatures) {
-navFeatures.addEventListener('mouseover', overFeatures); }
-if  (navMaterials) {
-navMaterials.addEventListener('mouseover', overMaterials); }
-if (navCertifications) {
-navCertifications.addEventListener('mouseover', overCertficiations); } 
-
-// code for ideas for every room page
+if (materialsnav != null) {
+	materialsnav.addEventListener('mouseover', showmaterials);
+}
 
 // code for quiz
 
 const questions = [
 	{
-		question: 'Which ice cream is the best?',
+		question: 'lorem blelelelelelelellelelelelelelelelellelele?',
 		answers: [
 			{ text: 'malinowe', correct: false },
 			{ text: 'bananowe', correct: false },
 			{ text: 'czekoladowe', correct: false },
 			{ text: 'pistacjowe', correct: true },
-		]
+		],
 	},
 	{
 		question: 'Which movie is the best?',
 		answers: [
-			{ text: 'Fargo', correct: false }, 
+			{ text: 'Fargo', correct: false },
 			{ text: 'Zootopia', correct: false },
 			{ text: 'Seksmisja', correct: false },
 			{ text: 'dupasraka', correct: true },
-		]
+		],
 	},
 
 	{
@@ -130,92 +100,75 @@ const questions = [
 			{ text: 'filetowy', correct: false },
 			{ text: 'czekoladowy', correct: false },
 			{ text: 'zielony', correct: true },
-		]
-	}
+		],
+	},
 ];
 
-
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
-
-
-
-
+const questionElement = document.getElementById('question');
+const answerButtons = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('next-btn');
 
 let currentQuestionIndex = 0;
 let score = 0;
 
- 
-
-
-function startQuiz(){
+function startQuiz() {
 	currentQuestionIndex = 0;
-	score = 0; 
-	nextButton.innerHTML ="Next";
+	score = 0;
+	nextButton.innerHTML = 'Next';
 	showQuestion();
 }
 
-
-function showQuestion(){
+function showQuestion() {
 	resetState();
 	let currentQuestion = questions[currentQuestionIndex];
 	let questionNo = currentQuestionIndex + 1;
-	questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+	questionElement.innerHTML = questionNo + '. ' + currentQuestion.question;
 
-
-	currentQuestion.answers.forEach(answer => { 
-		const button = document.createElement("button");
+	currentQuestion.answers.forEach((answer) => {
+		const button = document.createElement('button');
 		button.innerHTML = answer.text;
-		button.classList.add("btn");
+		button.classList.add('btn');
 		answerButtons.appendChild(button);
-		if(answer.correct) {
-			button.dataset.correct = answer.correct;  
+		if (answer.correct) {
+			button.dataset.correct = answer.correct;
 		}
-		button.addEventListener("click",selectAnswer);
-
+		button.addEventListener('click', selectAnswer);
 	});
 }
 
-
-
-function resetState(){
-	nextButton.style.display = "none";
-	while(answerButtons.firstChild) {
+function resetState() {
+	nextButton.style.display = 'none';
+	while (answerButtons.firstChild) {
 		answerButtons.removeChild(answerButtons.firstChild);
 	}
 }
 
-function selectAnswer(e){
-	const selectedBtn = e.target; 
-	const isCorrect = selectedBtn.dataset.correct === "true";
-	if (isCorrect){
-		selectedBtn.classList.add("correct");
+function selectAnswer(e) {
+	const selectedBtn = e.target;
+	const isCorrect = selectedBtn.dataset.correct === 'true';
+	if (isCorrect) {
+		selectedBtn.classList.add('correct');
 		score++;
-	}else{
-		selectedBtn.classList.add("incorrect"); 
+	} else {
+		selectedBtn.classList.add('incorrect');
 	}
-	Array.from(answerButtons.children).forEach(button=> {
-		if(button.dataset.correct === "true") {
-			button.classList.add("correct");
+	Array.from(answerButtons.children).forEach((button) => {
+		if (button.dataset.correct === 'true') {
+			button.classList.add('correct');
 		}
 		button.disabled = true;
 	});
-	nextButton.style.display = "block";
+	nextButton.style.display = 'block';
 }
-
 
 function showScore() {
 	resetState();
 	questionElement.innerHTML = `Your score is ${score} out of ${questions.length}`;
-	nextButton.innerHTML = "play again";
-	nextButton.style.display = "block";
-
+	nextButton.innerHTML = 'play again';
+	nextButton.style.display = 'block';
 }
 
-
-
-function handleNextButton () {
+function handleNextButton() {
 	currentQuestionIndex++;
 	if (currentQuestionIndex < questions.length) {
 		showQuestion();
@@ -224,12 +177,27 @@ function handleNextButton () {
 	}
 }
 
-nextButton.addEventListener("click", ()=>{
+nextButton.addEventListener('click', () => {
 	if (currentQuestionIndex < questions.length) {
 		handleNextButton();
 	} else {
 		startQuiz();
 	}
-})
+});
 startQuiz();
 
+const hamburger = document.querySelector('div.hamburger');
+const nav = document.querySelectorAll('ul.navigation');
+const sideNav = document.querySelector('div.sideNav');
+const heading1 = document.querySelector('p.second');
+const heading2 = document.querySelector('p.first');
+
+const navDown = () => {
+	sideNav.classList.toggle('show-nav');
+	heading1.classList.add('hide');
+	heading2.classList.add('hide');
+};
+
+if (hamburger != null) {
+	hamburger.addEventListener('click', navDown);
+}
